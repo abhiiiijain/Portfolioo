130f8c7da857875fb1fcdb3551b8bbd3ed10a48d
abhiiiijain <abhinandanbansal123@gmail.com>
2023-07-15 14:25:32 +0530

    resume icon added to main page

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -2,6 +2,7 @@ import {
   Github,
   Twitter,
   Instagram,
+  Document,
   Facebook,
   YouTube,
 } from "../components/AllSvgs";
@@ -111,6 +112,28 @@ const SocialIcons = (props) => {
           // to={{ pathname: "https://www.instagram.com/abhiiiijain" }}
         >
           <Instagram
+            width={25}
+            height={25}
+            fill={
+              props.theme === "dark" ? `${DarkTheme.text}` : `${DarkTheme.body}`
+            }
+          />
+        </a>
+      </motion.div>
+
+      {/* Document Icon */}
+      <motion.div
+        initial={{ scale: 0 }}
+        animate={{ scale: [0, 1, 1.5, 1] }}
+        transition={{ type: "spring", duration: 1, delay: 1.4 }}>
+        <a
+          style={{ color: "inherit" }}
+          target="_blank"
+          rel="noreferrer"
+          href={"https://drive.google.com/file/d/1EaWobkG910zq9kx2L21WCimAm5vWPRL-/view?usp=sharing"}
+          // to={{ pathname: "https://www.instagram.com/abhiiiijain" }}
+        >
+          <Document
             width={25}
             height={25}
             fill={
